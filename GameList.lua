0b8811a541d09079fc11cc35248fdccff6df1f08
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2026-04-23 18:02:42 +0800

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [4490140733] = "https://raw.githubusercontent.com/ditampol-tzuyoon/Ohdear/refs/heads/main/MR/main.lua", -- My Restaurant
+  [13200523806] = "https://raw.githubusercontent.com/ditampol-tzuyoon/Ohdear/refs/heads/main/MR/plaza.lua" -- Trading Plaza
+}
+
+return Games
